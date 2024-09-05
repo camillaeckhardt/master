54acdf71071d23e24571af190970538831568961
Camilla <camillaeckhardt@u.boisestate.edu>
2024-09-05 00:48:25 -0600

    one last commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main
       System.out.println("Hello World");
       System.out.println("yay");
       System.out.println("Hows your day");
+      System.out.println("last try");
     }
   }
